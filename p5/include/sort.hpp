8d2f529b70e9ea597ec3d21d3dc305dac585d04a
vanessavvp <vanessavvp2402@gmail.com>
2020-04-30 09:13:19 +0100

    Arreglos

--- p5/include/sort.hpp
+++ p5/include/sort.hpp
@@ -9,10 +9,15 @@
  * Purpose: Sorting methods
  */
 
-#include "../include/DNI.hpp"
+
+#ifndef INCLUDE_SORT_H
+#define INCLUDE_SORT_H
+
 #include <vector>
 #include <iostream>
+#include <cstdlib>
 
+#include "DNI.hpp"
 
 template <typename Clave> 
 void show(std::vector<Clave>& sequence) {  
@@ -22,14 +27,27 @@ void show(std::vector<Clave>& sequence) {
 }
 
 
-//---------------- Quadratic Sorting Methods ------------------------
+
+template <typename Clave>
+void printTrace(std::vector<Clave>& sequence, int aux1, int aux2) {  
+  std::string red_color = "31m";
+  for (int i = 0; i < sequence.size(); i++) {
+    if (i == aux1 || i == aux2) {
+      std::cout << "\033[0;"+red_color << sequence[i] << " \033[0m" << " ";
+    }
+    else std::cout << sequence[i] << " "; 
+  }
+  std::cout << "\n\n"; 
+  getchar();
+}
+
+//---------------- Sorting Methods ------------------------
 
 template <typename Clave> 
 std::vector<Clave> insertion(std::vector<Clave>& sequence, int size, int mode) {
   int j;
   Clave new_element_x;
-  int aux, aux2;
-  std::string red_color = "31m";
+  int aux1, aux2;
 
   for (int i = 1; i < size; i++) {
     j = i;
@@ -37,23 +55,15 @@ std::vector<Clave> insertion(std::vector<Clave>& sequence, int size, int mode) {
     
     while (new_element_x < sequence[j-1]) {
       sequence[j] = sequence[j-1];
-      aux = j;
+      aux1 = j;
       j--;
     }
     sequence[j] = new_element_x;
     aux2 = j;
 
     //Demonstration Mode = 1 ------------------------------------------------------
-    if (mode == 1) {
-      std::cout << "\nEvery modification will be shown on red\n";
-      for (int i = 0; i < sequence.size(); i++) {
-        if (i == aux || i == aux2) {
-          std::cout << "\033[0;"+red_color << sequence[i] << " \033[0m" << " ";
-        }
-        else std::cout << sequence[i] << " ";
-      }
-      std::cout << "\n";
-    } 
+    if (mode == 1) 
+      printTrace(sequence, aux1, aux2);
     //--------------------------------------------------------------------------------
   }
   return sequence;
@@ -63,29 +73,21 @@ std::vector<Clave> insertion(std::vector<Clave>& sequence, int size, int mode) {
 
 template <typename Clave> 
 std::vector<Clave> bubbleSort(std::vector<Clave>& sequence, int size, int mode) {
-  int aux, aux2;
+  int aux1, aux2;
   std::string red_color = "31m";
 
   for (int i = 1; i < size; i++) {
     for (int j = size-1; j >= i; j--) {
       if (sequence[j] < sequence[j-1]) {
         std::swap(sequence[j-1], sequence[j]);
-        aux = j-1;
+        aux1 = j-1;
         aux2 = j;
       }
     }
     //Demonstration Mode = 1 ------------------------------------------------------
-      if (mode == 1) {
-        std::cout << "\nEvery modification will be shown on red\n";
-        for (int k = 0; k < sequence.size(); k++) {
-          if (k == aux || k == aux2) {
-            std::cout << "\033[0;"+red_color << sequence[k] << " \033[0m" << " ";
-          }
-          else std::cout << sequence[k] << " ";
-        }
-        std::cout << "\n";
-      } 
-      //--------------------------------------------------------------------------------
+    if (mode == 1) 
+      printTrace(sequence, aux1, aux2);
+    //--------------------------------------------------------------------------------
   }
   return sequence;
 }
@@ -122,7 +124,7 @@ void heapify(int iterator, std::vector<Clave>& sequence, int size_seq) {
 template <typename Clave>
 std::vector<Clave> heapSort(std::vector<Clave>& sequence, int size, int mode) {
   int half_size = size/2;
-  int aux, aux2, iter;
+  int aux1, aux2, iter;
   std::string red_color = "31m";
 
   for (int i = half_size-1; i >= 0; i--) {
@@ -132,22 +134,14 @@ std::vector<Clave> heapSort(std::vector<Clave>& sequence, int size, int mode) {
   for (int i = size-1; i >= 0; i--) {
     std::swap(sequence[0], sequence[i]);
     heapify(0, sequence, i-1);
-    
-    aux = 0;
+
+    aux1 = 0;
     aux2 = i;
     //Demonstration Mode = 1 ------------------------------------------------------
     if (mode == 1) {
-      std::cout << "\nEvery modification will be shown on red\n";
-      for (int k = 0; k < sequence.size(); k++) {
-        if (k == aux || k == aux2) {
-          std::cout << "\033[0;"+red_color << sequence[k] << " \033[0m" << " ";
-        }
-        else std::cout << sequence[k] << " ";
-      }
-      std::cout << "\n";
+      printTrace(sequence, aux1, aux2);
     } 
-    //--------------------------------------------------------------------------------
-    
+    //-------------------------------------------------------------------------------- 
   }
   return sequence; 
 }
@@ -155,10 +149,12 @@ std::vector<Clave> heapSort(std::vector<Clave>& sequence, int size, int mode) {
 
 
 template <typename Clave>
-std::vector<Clave> quickSort(std::vector<Clave>& sequence, int begin, int end) {
+std::vector<Clave> quickSort(std::vector<Clave>& sequence, int begin, int end, int mode) {
+  int aux1, aux2;
   int new_begin = begin;
   int new_end   = end;
   Clave pivot = sequence[(new_begin + new_end) / 2];
+  std::string red_color = "31m";
   
   while (new_begin <= new_end) {
 
@@ -167,13 +163,20 @@ std::vector<Clave> quickSort(std::vector<Clave>& sequence, int begin, int end) {
 
     if (new_begin <= new_end) {
       std::swap(sequence[new_begin], sequence[new_end]);
+      aux1  = new_begin;
+      aux2 = new_end;
       new_begin++;
       new_end--;
     }
+
+    //Demonstration Mode = 1 ------------------------------------------------------
+    if (mode == 1) 
+      printTrace(sequence, aux1, aux2);
+    //--------------------------------------------------------------------------------
   }
 
-  if (begin < new_end) quickSort(sequence, begin, new_end);
-  if (new_begin < end) quickSort(sequence, new_begin, end);
+  if (begin < new_end) quickSort(sequence, begin, new_end, mode);
+  if (new_begin < end) quickSort(sequence, new_begin, end, mode);
 
   return sequence; 
 }
@@ -182,8 +185,10 @@ std::vector<Clave> quickSort(std::vector<Clave>& sequence, int begin, int end) {
 
 
 template<typename Clave>
-void deltaSort(int delta , std::vector<Clave>& sequence, int size) {
+void deltaSort(int delta, std::vector<Clave>& sequence, int size, int mode) {
   Clave x_data;
+  int aux1, aux2;
+  std::string red_color = "31m";
 
   for (int i = delta; i < size; i++) {
     x_data = sequence[i];
@@ -191,22 +196,31 @@ void deltaSort(int delta , std::vector<Clave>& sequence, int size) {
 
     while ((j >= delta) && (x_data < sequence[j-delta])) {
       sequence[j] = sequence[j-delta];
+      aux1 = j;
       j = j - delta;
     } 
     sequence[j] = x_data;
+    aux2 = j;
+
+    //Demonstration Mode = 1 ------------------------------------------------------
+    if (mode == 1) 
+      printTrace(sequence, aux1, aux2);
+    //-----------------------------------------------------------------------------
   } 
 }
 
 
 
 template <typename Clave>
-std::vector<Clave> shellSort(float alfa, std::vector<Clave>& sequence, int size) {
+std::vector<Clave> shellSort(float alfa, std::vector<Clave>& sequence, int size, int mode) {
   int delta = size * alfa;
   
   while (delta > 1){
     delta = delta / 2;
-    deltaSort(delta, sequence, size);
+    deltaSort(delta, sequence, size, mode);
   }
   
   return sequence;
 }
+
+#endif //INCLUDE_SORT_H
